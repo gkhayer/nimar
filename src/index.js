0965716070386f8f0cebb94c0204e8052596d6d2
Gagan K. Hayer <ghayer@iteamnm.com>
2017-11-22 08:21:33 -0700

    feat(pagination): add code for pagination

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App';
 import Home from './pages/Home';
-import About from './pages/About';
-import Memories from './pages/Memories';
-import registerServiceWorker from './registerServiceWorker';
 
 render(
-  <Router>
-    <div>
-      <Route path='/' component={App}></Route>
-    </div>
-  </Router>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'));
-registerServiceWorker();
