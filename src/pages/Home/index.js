dcfe22881582099632f8ccbe53ed343f9cf66616
Gagan K. Hayer <ghayer@iteamnm.com>
2018-01-05 16:41:15 -0700

    content(home): fix home photos- use flex

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -26,33 +26,33 @@ export default class Home extends React.Component {
           </div>
         </div>
 
-          <div className="notes-cards">
-          {
-            home.map((value, idx) => {
-              return (
-                <div key={`home${idx}`} className="flipcard">
-                  <div className="demo-card-square mdl-card mdl-shadow--2dp">
-                    <div className="mdl-card__title mdl-card--expand">
-                      <i style={{'color': 'pink'}} className="material-icons heart">favorite</i>
-                    </div>
-                    <div className="flip-container">
-                      <div className="flipper">
-                        <div className="front">
-                          <img className="home-img" src={value.imagefront} alt={value.timeline}/>
-                        </div>
-                        <div className="back">
-                          <img className="home-img" src={value.imageback} alt={value.timeline}/>
-                        </div>
+        <div className="notes-cards">
+        {
+          home.map((value, idx) => {
+            return (
+              <div key={`home${idx}`} className="flipcard">
+                <div className="demo-card-square mdl-card mdl-shadow--2dp">
+                  <div className="mdl-card__title mdl-card--expand card-title">
+                    <i style={{'color': 'pink'}} className="material-icons heart">favorite</i>
+                  </div>
+                  <div className="flip-container">
+                    <div className="flipper">
+                      <div className="front">
+                        <img className="home-img" src={value.imagefront} alt={value.timeline}/>
+                      </div>
+                      <div className="back">
+                        <img className="home-img" src={value.imageback} alt={value.timeline}/>
                       </div>
                     </div>
-                    <div className="mdl-card__supporting-text quote">{value.quote}</div>
-                    <div className="mdl-card__actions mdl-card--border"></div>
                   </div>
+                  <div className="mdl-card__supporting-text quote">{value.quote}</div>
+                  <div className="mdl-card__actions mdl-card--border"></div>
                 </div>
-            )})
-          }
-        </div>
+              </div>
+          )})
+        }
       </div>
+    </div>
     )
   }
 }
