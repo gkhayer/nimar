324d2d4314ace85265a169ee33421912808cfcff
unknown <gkhayer>
2018-05-31 13:49:52 -0600

    fix(pagination): spelling error

--- src/component/pagination/index.js
+++ src/component/pagination/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 
-import './Pagination.css';
+import './pagination.css';
 
 const propTypes = {
     items: PropTypes.array.isRequired,
