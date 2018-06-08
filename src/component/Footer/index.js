7882e18b54b1f8b1837ef5aae1b87afe0947bbbd
Gagan K. Hayer <ghayer@iteamnm.com>
2018-06-08 09:44:36 -0600

    style(Footer): fix footer styles

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 export default class Footer extends React.Component {
@@ -7,12 +7,16 @@ export default class Footer extends React.Component {
     render(){
         return (
           <div className="footer">
-            <div>This is a footer</div>
-            <div className="default"></div>
-            <div className="summer"></div>
-            <div className="winter"></div>
-            <div className="spring"></div>
-            <div className="autumn"></div>
+            <div>
+              <button className="button">preetkaur9016@gmail.com</button>
+              <span>408.714.8425</span>
+              <nav className="">
+                <Link to='/home' className="selected">Home</Link>
+                <Link to='/memories' className="selected">Memories</Link>
+                <Link to='/about' className="selected">About</Link>
+                <Link to='/contact' className="selected">Contact</Link>
+              </nav>
+            </div>
           </div>
         );
     }
