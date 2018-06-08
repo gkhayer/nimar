716b8b881520e1fe206f59282bf8b9d5f7053554
unknown <gkhayer>
2018-06-08 07:37:04 -0600

    content():add content

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -7,8 +7,8 @@ export default class Footer extends React.Component {
     render(){
         return (
           <div className="footer">
-            <div>This is a footer</div>
-            <div className="default"></div>
+            <div><button>preetkaur9016@gmail.com</button></div>
+            <div className="default">408-7148425</div>
             <div className="summer"></div>
             <div className="winter"></div>
             <div className="spring"></div>
